fe0396f2a924d0aa3674ce4ef46b6d484408bfbb
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2025-12-10 23:25:26 +0530

    Initial Commit

---
+++ src/main/java/com/jpa/mappings/dao/AppDAO.java
@@ -0,0 +1,12 @@
+package com.jpa.mappings.dao;
+
+import com.jpa.mappings.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+}
