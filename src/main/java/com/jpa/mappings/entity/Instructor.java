f26fbd97fa4e6da3fe81bdae6a8d352df6e2cae2
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2025-12-12 23:39:25 +0530

    Added annotation for Instructor entity

--- src/main/java/com/jpa/mappings/entity/Instructor.java
+++ src/main/java/com/jpa/mappings/entity/Instructor.java
@@ -3,6 +3,9 @@ package com.jpa.mappings.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "instructor")
 public class Instructor {
@@ -24,6 +27,9 @@ public class Instructor {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
+
+    @OneToMany(mappedBy = "instructor", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    private List<Course> courses;
 
     public Instructor() {
     }
@@ -74,8 +80,25 @@ public class Instructor {
         this.instructorDetail = instructorDetail;
     }
 
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
     @Override
     public String toString() {
         return "Instructor{" + "id=" + id + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", email='" + email + '\'' + ", instructorDetail=" + instructorDetail + '}';
+    }
+
+    public void add(Course tempCourse) {
+        if (courses == null) {
+            courses = new ArrayList<>();
+        }
+
+        courses.add(tempCourse);
+        tempCourse.setInstructor(this);
     }
 }
