9db58db3eb9fe932fccbb118ed2b1da6632bc3b7
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2025-12-13 23:56:33 +0530

    Created JOIN FETCH Query

--- src/main/java/com/jpa/mappings/dao/AppDAOImpl.java
+++ src/main/java/com/jpa/mappings/dao/AppDAOImpl.java
@@ -64,4 +64,19 @@ public class AppDAOImpl implements AppDAO {
         List<Course> courses = query.getResultList();
         return courses;
     }
+
+    @Override
+    public Instructor findInstructorByIdJoinFetch(int theId) {
+        TypedQuery<Instructor> query = entityManager.createQuery(
+                     "select i from Instructor i "
+                        + "JOIN FETCH i.courses "
+                        + "where i.id = :data", Instructor.class
+        );
+        query.setParameter("data", theId);
+
+        Instructor instructor = query.getSingleResult();
+        System.out.println("findInstructorByIdJoinFetch() instructor = " + instructor);
+
+        return instructor;
+    }
 }
